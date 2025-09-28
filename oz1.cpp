53f6afdc281eb25df9f9a79c1314ae43029534b1
gati04 <Ibragim-gadjiev.04@yandex.ru>
2025-09-28 21:52:27 +0300

    Add files via upload

---
+++ oz1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(0, "");
+	double a, b, c;
+	cout << "Количество часов: "; cin >> a;
+	cout << "Ставка: "; cin >> b;
+	cout << "Процент: "; cin >> c;
+	cout << "Заработанная сумма: " << a * b + (a * b * c) / 100;
+	return 0;
+}
